62e068730b00fd7bac8ad859704eaa6dc0ae068e
Alex <livalex11@gmail.com>
2018-04-14 01:32:28 +0300

    #response -add

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
@@ -11,9 +12,17 @@ $request = RequestFactory::fromGlobals();
 
 # Action
 
-
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-header('X-Developer: livalex');
-echo "Hello, " . $name . "!" . PHP_EOL;
+$response = (new Response('Hello ' . $name . '!'))
+            ->withHeader('X-Developer', 'livalex');
+
+# Sending
+
+header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+
+echo $response->getBody();
 
