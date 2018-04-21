68dd22f1fbb06dba16c7e5828be0daa2bbdfd2b3
Alex <livalex11@gmail.com>
2018-04-21 11:12:27 +0300

    #routing -add Simple example of the blog's routes

--- public/index.php
+++ public/index.php
@@ -28,6 +28,19 @@ if ($path === '/') {
     $response = new HtmlResponse('Hello ' . $name . '!');
 } elseif ($path === '/about') {
     $response = new HtmlResponse("I'm a simple site");
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => "I'm a second post"],
+        ['id' => 1, 'title' => "I'm a first post"],
+    ]);
+} elseif (preg_match('/^\/(?P<name>blog)\/(?P<id>\d+)$/i', $path, $matches)) {
+    if ($matches['id'] == 1) {
+        $response = new HtmlResponse("<h1>Hello!</h1><p>I'm a first post</p>");
+    } elseif ($matches['id'] == 2) {
+        $response = new HtmlResponse("<h1>Hello!</h1><p>I'm a second post</p>");
+    } else {
+        $response = new JsonResponse(['error' => 'Page is not exist']);
+    }
 } else {
     $response = new JsonResponse(['error' => 'Undefined page'], 404);
 }
