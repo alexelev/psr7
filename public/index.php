c2dad0366b9f832637add73c779bfddf8c760133
Alex <livalex11@gmail.com>
2018-06-02 13:22:49 +0300

    #routes -fix

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\RouteCollection;
 use Framework\Http\Router\Router;
-use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\HtmlResponse;
+use Http\Controller\AboutAction;
+use Http\Controller\Blog\IndexAction;
+use Http\Controller\Blog\ShowAction;
+use Http\Controller\HomeAction;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\SapiEmitter;
@@ -23,32 +25,10 @@ function dd($d)
 # Initialization
 
 $routes = new RouteCollection();
-
-$routes->get('home', '/', function (ServerRequestInterface $request) {
-    $name = $request->getQueryParams()['name'] ?? 'Guest';
-    return new HtmlResponse('Hello ' . $name . '!');
-});
-
-$routes->get('about', '/about', function () {
-    return new HtmlResponse("I'm a simple site");
-});
-
-$routes->get('blog', '/blog', function () {
-    return new JsonResponse([
-        ['id' => 2, 'title' => "I'm a second post"],
-        ['id' => 1, 'title' => "I'm a first post"],
-    ]);
-});
-
-$routes->get('blog_show', '/blog/{id}', function (ServerRequestInterface $request) {
-    $id = $request->getAttribute('id');
-    if ($id > 3) {
-        return new JsonResponse(['error' => 'Undefined page', 404]);
-    }
-    return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
-},
-    ['id' => '\d+']
-);
+$routes->get('home', '/', new HomeAction());
+$routes->get('about', '/about', new AboutAction());
+$routes->get('blog', '/blog', new IndexAction());
+$routes->get('blog_show', '/blog/{id}', new ShowAction(), ['id' => '\d+']);
 
 $router = new Router($routes);
 $request = ServerRequestFactory::fromGlobals();
