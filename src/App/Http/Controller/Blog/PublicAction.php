e5760be180fa0163c22650643eb54b0fbfe6bd83
Alex <livalex11@gmail.com>
2018-06-09 12:06:57 +0300

    #publicAction @controller -add

---
+++ src/App/Http/Controller/Blog/PublicAction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controller\Blog;
+
+use Framework\Http\Router\Router;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Diactoros\Response\RedirectResponse;
+
+class PublicAction
+{
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function __invoke(ServerRequestInterface $request)
+    {
+        $id = $request->getAttribute('id');
+
+        if ($id > 3) {
+            return new JsonResponse(['error' => 'Undefined page'], 404);
+        }
+
+        return new RedirectResponse($this->router->generate('blog_show', ['id' => $id]));
+    }
+}
